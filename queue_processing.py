9a744d2a24c03f3e3db3b863ae9f38c75ddbecba
minhnhatdo <minhnhatdo0709@gmail.com>
2024-06-21 20:47:58 +0700

    dynamic port

--- queue_processing.py
+++ queue_processing.py
@@ -3,10 +3,14 @@ import os
 from comfyapp import ComfyUI
 import redis
 import time
+import sys
 
 def main():
   comfyUI = ComfyUI()
-  comfyUI.prepare_comfyui()
+
+  # get the port from the first command line
+  port = sys.argv[1]
+  comfyUI.prepare_comfyui(port)
   time.sleep(15)
 
   redis_uri = os.environ.get('REDIS_URI')
@@ -19,7 +23,7 @@ def main():
 
       if task:
         task = json.loads(task)
-        comfyUI.run_task(task)
+        comfyUI.run_task(task, port)
     except Exception as e:
       print(e)
 
